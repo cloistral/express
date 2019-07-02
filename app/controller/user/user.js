c995f106acbb72e601cde874465e9ebdc3fee6df
apple <707107790@qq.com.com>
2019-07-02 16:34:53 +0800

    change

--- app/controller/user/user.js
+++ app/controller/user/user.js
@@ -1,5 +1,10 @@
+
+var User = require('../../model/user.model')
 module.exports = {
     getUserInfo(req,res,next){
+        User.find().then((data) => {
+            console.log(data)
+        })
         console.log(req.user)
         res.json(req.user);
     }
